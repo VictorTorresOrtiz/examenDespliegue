2887a2faa866d2810465898cfb3073470d97b693
Victor Torres Ortiz <victor.torres.ortiz.18@gmail.com>
2022-11-27 19:39:14 +0100

    Cambios a juegos

--- examenPHP/Juego.php
+++ examenPHP/Juego.php
@@ -1,28 +1,28 @@
-<?php 
-class Juego extends Soporte {
-
-
+<?php
+include "Soporte.php";
+class Juego extends Soporte{
     public $consola;
-    public $minNumJugadores;
-    public $maxNumJugadores;
-    public $muestraJugadoresPosibles = array();
-
-    public function getTitulo()
-    {
-        return  $this->titulo;
+    private $minNumJugadores;
+    private $maxNumJugadores;
+    public function __construct($titulo,$numero,$precio,$consola,$minNumJugadores,$maxNumJugadores){
+        parent::__construct($titulo,$numero,$precio);
+        $this->consola=$consola;
+        $this->minNumJugadores=$minNumJugadores;
+        $this->maxNumJugadores=$maxNumJugadores;
     }
-
-    public function __construct($titulo, $consola, $minNumJugadores, $maxNumJugadores, $muestraJugadoresPosibles = array()) {
-        $this->titulo = $titulo;
-        $this->consola = $consola;
-        $this->minNumJugadores = $minNumJugadores;
-        $this->maxNumJugadores = $maxNumJugadores;
-        $this->$muestraJugadoresPosibles[] = $muestraJugadoresPosibles;
-
+    public function muestraJugadoresPosibles(){
+        if($this->minNumJugadores==1 && $this->maxNumJugadores==1){
+            echo "Dedicado a  un jugador";
+        }else if($this->minNumJugadores==$this->maxNumJugadores){
+            echo "Para $this->minNumJugadores jugadores";
+        }else{
+            echo "Desde $this->minNumJugadores hasta $this->maxNumJugadores jugadores";
+        }
     }
- 
-
-
-
-
+    public function muestraResumen(){
+        parent:: muestraResumen();
+        echo "<br>Consola: $this->consola";
+        echo $this->muestraJugadoresPosibles(); 
+    }
 }
+?>
