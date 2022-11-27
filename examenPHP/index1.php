7357597c7d941963428720acffe2961d4ca9ae46
Victor Torres Ortiz <victor.torres.ortiz.18@gmail.com>
2022-11-27 19:38:12 +0100

    Cambios index1

--- examenPHP/index1.php
+++ examenPHP/index1.php
@@ -7,4 +7,12 @@ echo "<strong>" . $soporte1->titulo . "</strong>";
 echo "<br>Precio: " . $soporte1->getPrecio() . " euros"; 
 echo "<br>Precio IVA : " . $soporte1->getPrecioConIVA() . " euros";
 $soporte1->muestraResumen();
+
+include "CintaVideo.php";
+
+$miCinta = new CintaVideo("Los Mataos", 23, 3.5, 107); 
+echo "<strong>" . $miCinta->titulo . "</strong>"; 
+echo "<br>Precio: " . $miCinta->getPrecio() . " euros"; 
+echo "<br>Precio IVA: " . $miCinta->getPrecioConIva() . " euros";
+$miCinta->muestraResumen();
 */    
